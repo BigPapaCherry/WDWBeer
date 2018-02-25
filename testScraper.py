96746ee0bbee04adfcc17ebe0d75008e98380c39
Tom Cherry <thomas.p.cherry@gmail.com>
2018-02-25 10:35:03 -0500

    Removed comments. Covers most beer scenarios in an individual menu

--- testScraper.py
+++ testScraper.py
@@ -16,23 +16,12 @@ names = []
 prices = []
 for table in soup.findAll('div', attrs={'class': 'group', 'id': 'Alcoholic Beverage'}):
     title = table.find('h3', attrs={'class': 'group-title'}).text
-    # if any(type in title for type in beerTypes):
     for item in table.findAll('div', attrs={'class': 'item'}):
         name = item.find('h4', attrs={'class': 'item-title'}).text
         price = item.findAll('div', attrs={'class': 'price-value'})[1]
         if any(type in title for type in beerTypes) or any(type in name for type in beerTypes):
             name = re.sub('((\d\d-)|(-)|(,)).*', '', name).strip()
             names.append(name)
-    # else:
-    #     for item in table.findAll('div', attrs={'class': 'item'}):
-    #
-    #         name = item.find('h4', attrs={'class': 'item-title'}).text
-    #         price = item.findAll('div', attrs={'class': 'price-value'})[1]
-    #
-    #         if any(type in name for type in beerTypes):
-    #             name = name.split(sep, 1)[0]
-    #             name = name.split(sep2, 1)[0]
-    #
-    #             names.append(name)
+
 
 pprint(list(set(names)))
