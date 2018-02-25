10e466e7e047f1327e5e44bdf3df9c34f4fc0fc1
Tom Cherry <thomas.p.cherry@gmail.com>
2018-02-25 10:34:16 -0500

    Using Abracadabar, stripping unnecessary fluff from beer name, and pulling all beers from section with beer section header

--- testScraper.py
+++ testScraper.py
@@ -3,10 +3,12 @@ from pprint import pprint
 from BeautifulSoup import BeautifulSoup
 import re
 
-url = 'https://disneyworld.disney.go.com/dining/hollywood-studios/baseline-tap-house/menus/'
+url = 'https://disneyworld.disney.go.com/dining/boardwalk/abracadabar/menus/'
 response = requests.get(url)
 html = response.content
 beerTypes = ['IPA', 'Ale', 'Lager', 'Stout', 'Pilsner', 'Pils', 'Porter', 'Hefeweizen', 'Beer']
+sep = ','
+sep2 = '-'
 
 soup = BeautifulSoup(html)
 
@@ -14,19 +16,23 @@ names = []
 prices = []
 for table in soup.findAll('div', attrs={'class': 'group', 'id': 'Alcoholic Beverage'}):
     title = table.find('h3', attrs={'class': 'group-title'}).text
-    if any(type in title for type in beerTypes):
-        for item in table.findAll('div', attrs={'class': 'item'}):
-
-            name = item.find('h4', attrs={'class': 'item-title'}).text
-            pprint('Title Said Beer')
+    # if any(type in title for type in beerTypes):
+    for item in table.findAll('div', attrs={'class': 'item'}):
+        name = item.find('h4', attrs={'class': 'item-title'}).text
+        price = item.findAll('div', attrs={'class': 'price-value'})[1]
+        if any(type in title for type in beerTypes) or any(type in name for type in beerTypes):
+            name = re.sub('((\d\d-)|(-)|(,)).*', '', name).strip()
             names.append(name)
-    else:
-        for item in table.findAll('div', attrs={'class': 'item'}):
-
-            name = item.find('h4', attrs={'class': 'item-title'}).text
-            price = item.findAll('div', attrs={'class': 'price-value'})[1]
-
-            if any(type in name for type in beerTypes):
-                names.append(name)
+    # else:
+    #     for item in table.findAll('div', attrs={'class': 'item'}):
+    #
+    #         name = item.find('h4', attrs={'class': 'item-title'}).text
+    #         price = item.findAll('div', attrs={'class': 'price-value'})[1]
+    #
+    #         if any(type in name for type in beerTypes):
+    #             name = name.split(sep, 1)[0]
+    #             name = name.split(sep2, 1)[0]
+    #
+    #             names.append(name)
 
 pprint(list(set(names)))
